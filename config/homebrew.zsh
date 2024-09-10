041c11e4c0e37f6482df76c219cbb197088cac5f
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2024-09-10 18:27:54 -0400

    refactor(homebrew): reduce logging severity if missing

--- config/homebrew.zsh
+++ config/homebrew.zsh
@@ -1,7 +1,6 @@
-# Pre-flight checks
-if [ ! -d "/opt/homebrew" ]; then
-  # Always print this if missing, don't defer to DOT_FILES_DEBUG.
-  echo "\033[0;31mCannot find the Homebrew installation. It can be installed via https://brew.sh/\033[0m" >&2
+TARGET="/opt/homebrew"
+if [ ! -d "$TARGET" ]; then
+  [ "$DOT_FILES_DEBUG" ] && echo "$TARGET not found, skipping initialization."
   return
 fi
 
