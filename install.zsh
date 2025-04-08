75e064cbf9fe7f4e1333d3e6866a52cbb183aa60
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2025-04-08 18:42:49 -0400

    fix(install): non-Homebrew installs shown as done too early

--- install.zsh
+++ install.zsh
@@ -63,9 +63,9 @@ if [ "$INSTALL_AUTO_CONFIG_DEPS" = "y" ]; then
   echo "  Installing non-Homebrew packages..."
   # pnpm
   [ -d "$HOME/Library/pnpm" ] || curl -fsSL https://get.pnpm.io/install.sh | sh -
-  echo "  Done."
   # bun
   [ -f "$HOME/.bun/bin/bun" ] || curl -fsSL https://bun.sh/install | bash
+  echo "  Done."
 
   echo "  Installing Hombrew packages..."
   # Homebrew
